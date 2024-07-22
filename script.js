0a9c4aa8822c80a6a96a24552ac9c09e801e5a57
Jakub Lunga <jakub@189.sub-97-12-193.myvzw.com>
2024-07-22 15:23:46 +0200

    adding pdf text and sidebar

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-// script.js
+function showSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'block';
+}
+
+function hideSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'none';
+}
+
 function showSection(sectionId) {
     // Hide the hero section
     document.getElementById('hero').style.display = 'none';
-    
+
     // Hide all sections
     document.querySelectorAll('.content-section').forEach(section => {
         section.style.display = 'none';
@@ -11,6 +20,13 @@ function showSection(sectionId) {
     // Show the selected section
     document.getElementById(sectionId).style.display = 'block';
 
+    // Show or hide the sidebar based on the section
+    if (sectionId === 'guides') {
+        showSidebar();
+    } else {
+        hideSidebar();
+    }
+
     // Update active link
     document.querySelectorAll('header nav ul li a').forEach(link => {
         link.classList.remove('active');
@@ -18,7 +34,6 @@ function showSection(sectionId) {
     document.querySelector(`header nav ul li a[onclick="showSection('${sectionId}')"]`).classList.add('active');
 }
 
-// Show the hero section when clicking on "Home"
 function showHome() {
     document.getElementById('hero').style.display = 'block';
     document.querySelectorAll('.content-section').forEach(section => {
@@ -27,5 +42,33 @@ function showHome() {
     document.querySelectorAll('header nav ul li a').forEach(link => {
         link.classList.remove('active');
     });
-    document.querySelector(`header nav ul li a[onclick="showSection('hero')"]`).classList.add('active');
+    document.querySelector('header nav ul li a[onclick="showHome()"]').classList.add('active');
+    hideSidebar(); // Ensure the sidebar is hidden when going home
+}
+// Modal picture - zoom
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the modal image and caption elements
+var modalImg = document.getElementById("imgModal");
+var captionText = document.getElementById("caption");
+
+// Get all images with the class "zoomable"
+var images = document.getElementsByClassName("zoomable");
+
+// Add click event listener to each image
+for (var i = 0; i < images.length; i++) {
+    images[i].onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    };
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+    modal.style.display = "none";
 }
